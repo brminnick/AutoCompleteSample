d4909f14f85a264fee722222b35f6d8c6247726a
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-03-26 09:15:48 -0700

    Add Syncfusion.Xamarin.SfAutoComplete

---
+++ AutoCompleteSample/AutoCompletePage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Syncfusion.SfAutoComplete.XForms;
+using Xamarin.CommunityToolkit.Markup;
+using Xamarin.Forms;
+
+namespace AutoCompleteSample
+{
+    class AutoCompletePage : ContentPage
+    {
+        public AutoCompletePage()
+        {
+            Content = new SfAutoComplete
+            {
+                HeightRequest = 40,
+                AutoCompleteSource = new List<string>
+                {
+                    "Edge",
+                    "Firefox",
+                    "Chrome",
+                    "Opera",
+                    "Safari",
+                }
+            }.CenterVertical().FillExpandHorizontal();
+        }
+    }
+}
